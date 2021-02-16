9437c71373a66c56c043c1dbd40a66bd077ae34d
atsushi-ota <atsushi-ota@united.jp>
2021-02-17 00:03:27 +0900

    めっちゃはやい

--- src/stages/010/Field.ts
+++ src/stages/010/Field.ts
@@ -6,15 +6,29 @@ import { pixi_tilemap } from '@/index'
 export class Field extends PIXI.Container {
   private bgLayerContainer!: PIXI.Container
   private debugLayerContainer!: PIXI.Container
-  private horizontalGridNum = 200
-  private verticalGridNum = 200
+  private horizontalGridNum = 500
+  private verticalGridNum = 500
   private step = 0
   constructor(private texture: PIXI.Texture) {
     super()
     this.sortableChildren = true
     console.log(pixi_tilemap)
     pixi_tilemap.Constant.maxTextures = 4;
+    const fieldTexture = texture.clone()
+    fieldTexture.frame = new PIXI.Rectangle(0, 0, 16, 16)
+    var tilemap = new pixi_tilemap.CompositeRectTileLayer(0, [texture]);
+    // tilemap.addChild(PIXI.Sprite.from(texture))
+    // 地面
+    for (let y = 0; y < this.verticalGridNum; ++y) {
+      for (let x = 0; x < this.horizontalGridNum; ++x) {
+        const fieldTexture = texture.clone()
+        fieldTexture.frame = new PIXI.Rectangle(16 * Math.floor(Math.random() * 16), 16 * Math.floor(Math.random() * 8), 16, 16)
+        tilemap.addFrame(fieldTexture, x * 16, y * 16)
+      }
+    }
+    this.addChild(tilemap)
 
+    /*
     this.bgLayerContainer = new PIXI.ParticleContainer(20000, { uvs: false })
     // this.bgLayerContainer = new PIXI.Container()
     this.debugLayerContainer = new PIXI.Container()
@@ -31,6 +45,7 @@ export class Field extends PIXI.Container {
         this.bgLayerContainer.addChild(fieldSprite)
       }
     }
+    */
     this.on('added', () => {
       // 親に追加されたときになにかやるならここで
     })
