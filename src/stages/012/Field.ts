304d1cd9501f0b5211c73cde8b90c8eae711f389
atsushi-ota <atsushi-ota@united.jp>
2021-02-28 21:54:42 +0900

    衝突判定をTiledMapEditorで設定するように。

--- src/stages/012/Field.ts
+++ src/stages/012/Field.ts
@@ -3,6 +3,18 @@ import { SortableParticleContainer } from '@/stages/005/SortableParticleContaine
 import * as PIXI from "pixi.js"
 import * as PIXITilemap from '@/pixi-tilemap/index'
 
+const AREA_DIVIDE_GRID_NUM = 3
+
+class Collosion {
+  constructor(
+    public type: number,
+    public left: number,
+    public top: number,
+    public right: number,
+    public bottom: number
+  ) {}
+}
+
 export class Field extends PIXI.Container {
   private bgLayerContainer!: PIXITilemap.CompositeRectTileLayer
   private airLayerContainer!: PIXITilemap.CompositeRectTileLayer
@@ -13,6 +25,8 @@ export class Field extends PIXI.Container {
   private targetCharacter: Character | null = null
   private characters: Array<Character> = []
   private walls: Array<PIXI.Rectangle> = []
+  private collisions: Array<Collosion> = []
+  private collisionsByArea: Map<string, Array<Collosion>> = new Map() 
   private textureMap: Map<string, PIXI.Texture> = new Map()
   private textureList: Array<PIXI.Texture> = []
   constructor(private texture: PIXI.Texture, private mapChipData: any, private mapData: any) {
@@ -93,8 +107,38 @@ export class Field extends PIXI.Container {
               this.airLayerContainer.addFrame(this.textureList[textureIndex - 1], gridX * this.mapChipData.tilewidth, gridY * this.mapChipData.tileheight)
             }
           }
+          // 衝突判定
+          {
+            // 衝突判定用チップの開始indexを取得しておく
+            const firstIndex = this.mapData.tilesets.filter((v: any) => v.source === 'collosion.json')[0].firstgid
+            const textureIndex = this.mapData.layers[4].data[mapChipIndex]
+            if (textureIndex > 0) {
+              this.collisions[gridY * this.horizontalGridNum + gridX] = new Collosion(
+                textureIndex - firstIndex,
+                gridX * this.mapData.tilewidth,
+                gridY * this.mapData.tileheight,
+                gridX * this.mapData.tilewidth + this.mapData.tilewidth,
+                gridY * this.mapData.tileheight + this.mapData.tileheight
+              )
+            }
+          }
         }
       }
+      // 衝突判定を4x4グリッド毎にエリア分け
+      for (let gridY = 0; gridY < this.verticalGridNum; ++gridY) {
+        for (let gridX = 0; gridX < this.horizontalGridNum; ++gridX) {
+          const collision = this.collisions[gridY * this.horizontalGridNum + gridX]
+          if (collision != null) {
+            const areaGridString = [Math.floor(gridX / AREA_DIVIDE_GRID_NUM), Math.floor(gridY / AREA_DIVIDE_GRID_NUM)].toString()
+            if (this.collisionsByArea.has(areaGridString)) {
+              this.collisionsByArea.get(areaGridString)!.push(collision)
+            } else {
+              this.collisionsByArea.set(areaGridString, [collision])
+            }
+          }
+        }
+      }
+      console.log(this.collisionsByArea.size)
     }
   }
   public addCharacter(character: Character, isTarget = false) {
@@ -110,17 +154,6 @@ export class Field extends PIXI.Container {
   public update() {
     // preUpdate
     this.characters.forEach(chara => chara.preUpdate())
-    // update
-    this.characters.forEach(chara => {
-      if (chara.preUpdateInfo != null) {
-        const [moveX, moveY] = [chara.preUpdateInfo.moveX, chara.preUpdateInfo.moveY]
-        chara.x += moveX
-        chara.y += moveY
-        chara.currentDirection = chara.preUpdateInfo.nextDirection
-        chara.preUpdateInfo = null
-      }
-      chara.update()
-    })
     // 衝突判定など、ゲームの世界の都合でpreUpdateの内容に干渉しつつ、確定させる。
     this.characters.forEach(chara => {
       if (chara.preUpdateInfo != null) {
@@ -171,10 +204,10 @@ export class Field extends PIXI.Container {
     })
 
     // 視点移動
-    const rightLimitX = 649 - 128
-    const leftLimitX = 128
-    const bottomLimitY = 480 - 96
-    const topLimitY = 96
+    const rightLimitX = 649 - 160
+    const leftLimitX = 160
+    const bottomLimitY = 480 - 128
+    const topLimitY = 128
     if (this.targetCharacter) {
       const offsetX = this.targetCharacter.x + this.x
       if (offsetX > rightLimitX) {
@@ -214,33 +247,59 @@ export class Field extends PIXI.Container {
     })
     return [hit, hit ? hitDistance / Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2)) : 0]
   }
-  // 壁との衝突判定
   private hitWall(targetCharacter: Character, offsetX: number, offsetY: number): [boolean, number, number] {
+    const [gridX, gridY] = [Math.floor(targetCharacter.x / this.mapData.tilewidth), Math.floor(targetCharacter.y / this.mapData.tileheight)]
+    const areaGrid = [Math.floor(gridX / AREA_DIVIDE_GRID_NUM), Math.floor(gridY / AREA_DIVIDE_GRID_NUM)]
+    const collisions = this.getCollisionsByAreaGrid(areaGrid)
+    console.log(collisions.length)
+
     const targetRect = targetCharacter.hitRect.clone()
     targetRect.x += offsetX
     targetRect.y += offsetY
     let excessX = 0 // めり込んだ量X
     let excessY = 0 // めり込んだ量Y
-    const hit = this.walls.some(wall => {
-      const hHit = targetRect.left < wall.right && targetRect.right > wall.left
-      const vHit = targetRect.top < wall.bottom && targetRect.bottom > wall.top
+
+    // そのキャラの周辺のcollisionを抽出
+    const hit = collisions.some(collision => {
+      const hHit = targetRect.left < collision.right && targetRect.right > collision.left
+      const vHit = targetRect.top < collision.bottom && targetRect.bottom > collision.top
       const hit = hHit && vHit
       // 衝突した場合はめり込み量計算
       if (hit) {
         if (offsetX < 0) {
-          excessX = targetRect.left - wall.right
+          excessX = targetRect.left - collision.right
         } else if (offsetX > 0) {
-          excessX = targetRect.right - wall.left
+          excessX = targetRect.right - collision.left
         }
         if (offsetY < 0) {
-          excessY = targetRect.top - wall.bottom
+          excessY = targetRect.top - collision.bottom
         } else if (offsetY > 0) {
-          excessY = targetRect.bottom - wall.top
+          excessY = targetRect.bottom - collision.top
         }
       }
       return hit
     })
     return [hit, excessX, excessY]
+  }
+  private getCollisionsByAreaGrid(areaGrid: Array<number>): Array<Collosion> {
+    const collisions: Array<Collosion> = []
+    const areaGridStrings = [
+      areaGrid.toString(),
+      [areaGrid[0] - 1, areaGrid[1]].toString(),
+      [areaGrid[0] - 1, areaGrid[1] - 1].toString(),
+      [areaGrid[0], areaGrid[1] - 1].toString(),
+      [areaGrid[0] + 1, areaGrid[1] - 1].toString(),
+      [areaGrid[0] + 1, areaGrid[1]].toString(),
+      [areaGrid[0] + 1, areaGrid[1] + 1].toString(),
+      [areaGrid[0], areaGrid[1] + 1].toString(),
+      [areaGrid[0] - 1, areaGrid[1] + 1].toString()
+    ]
+    areaGridStrings.forEach(areaGridString => {
+      if (this.collisionsByArea.has(areaGridString)) {
+        Array.prototype.push.apply(collisions, this.collisionsByArea.get(areaGridString)!)
+      }
+    })
+    return collisions
   }
   public setDebugMode(flag: boolean) {
     this.debugLayerContainer.visible = flag
