d57cf902b198adfee1ee57d1e69f7a3822e6cf27
太田敦士 <atsushi-ota@united.jp>
2023-12-07 15:59:17 +0900

    2つのショットの出しわけ

--- src/game/02/routine/PlayerRoutine.ts
+++ src/game/02/routine/PlayerRoutine.ts
@@ -1,4 +1,4 @@
-import { KEY_CODE_LEFT, KEY_CODE_DOWN, KEY_CODE_RIGHT, KEY_CODE_SHOT, KEY_CODE_UP, GAME_PAD_DOWN, GAME_PAD_LEFT, GAME_PAD_RIGHT, GAME_PAD_SHOT, GAME_PAD_UP } from "../Game"
+import { KEY_CODE_LEFT, KEY_CODE_DOWN, KEY_CODE_RIGHT, KEY_CODE_L_SHOT, KEY_CODE_R_SHOT, KEY_CODE_UP, GAME_PAD_DOWN, GAME_PAD_LEFT, GAME_PAD_RIGHT, GAME_PAD_L_SHOT, GAME_PAD_R_SHOT, GAME_PAD_UP } from "../Game"
 import { Actor } from "../actor/Actor"
 import { SimpleAppearance } from "../appearance/SimpleAppearance"
 import { InputManager } from "../useInputManager"
@@ -9,7 +9,7 @@ export class PlayerRoutine extends RoutineBase {
   constructor (private inputManager: InputManager) {
     super()
   }
-  *generateFire () {
+  *generateLFire () {
     let nextFireFrame = 0
     while (true) {
       if (nextFireFrame === 0) {
@@ -23,8 +23,23 @@ export class PlayerRoutine extends RoutineBase {
       yield
     }
   }
+  *generateRFire () {
+    let nextFireFrame = 0
+    while (true) {
+      if (nextFireFrame === 0) {
+        const bullet = new Actor(new SimpleAppearance('/images/game/02/shot_red.png'), new BulletRoutine(), this.actor.game)
+        bullet.position.set(this.actor.x + 10, this.actor.y)
+        this.actor.game.addActor(bullet, 'playerShot')
+        nextFireFrame = 4
+      } else {
+        nextFireFrame--
+      }
+      yield
+    }
+  }
   *generateUpdate () {
-    const fire = this.generateFire()
+    const lFire = this.generateLFire()
+    const rFire = this.generateRFire()
     while (true) {
       if (this.inputManager.isPressing(KEY_CODE_LEFT) || this.inputManager.isPressingGamepad(GAME_PAD_LEFT)) {
         this.actor.x -= 2
@@ -38,8 +53,11 @@ export class PlayerRoutine extends RoutineBase {
       if (this.inputManager.isPressing(KEY_CODE_DOWN) || this.inputManager.isPressingGamepad(GAME_PAD_DOWN)) {
         this.actor.y += 2
       }
-      if (this.inputManager.isPressing(KEY_CODE_SHOT) || this.inputManager.isPressingGamepad(GAME_PAD_SHOT)) {
-        fire.next()
+      if (this.inputManager.isPressing(KEY_CODE_L_SHOT) || this.inputManager.isPressingGamepad(GAME_PAD_L_SHOT)) {
+        lFire.next()
+      }
+      else if (this.inputManager.isPressing(KEY_CODE_R_SHOT) || this.inputManager.isPressingGamepad(GAME_PAD_R_SHOT)) {
+        rFire.next()
       }
       yield
     }
