d13070badc09df09f9a2bb88a7b86dbcd7a9b9c7
atsushi-ota <atsushi-ota@united.jp>
2023-12-12 22:44:26 +0900

    レーザー実装

--- src/game/02/actor/Actor.ts
+++ src/game/02/actor/Actor.ts
@@ -5,11 +5,13 @@ import { RoutineBase } from "../routine/RoutineBase"
 export class Actor extends PIXI.Container {
   private routineUpdater: Generator | null = null
   private appearanceUpdater: Generator | null = null
-  constructor (private appearance: AppearanceBase, private routine: RoutineBase, public game: Game) {
+  constructor (private appearance: AppearanceBase, private routine: RoutineBase | null, public game: Game) {
     super()
     appearance.setup(this)
-    this.routine.actor = this
-    this.routineUpdater = this.routine.generateUpdate()
+    if (this.routine != null) {
+      this.routine.actor = this
+      this.routineUpdater = this.routine.generateUpdate()
+    }
     this.appearanceUpdater = this.appearance.generateUpdate()
   }
   update (delta: number) {
--- src/game/02/routine/PlayerRoutine.ts
+++ src/game/02/routine/PlayerRoutine.ts
@@ -1,16 +1,19 @@
 import { KEY_CODE_LEFT, KEY_CODE_DOWN, KEY_CODE_RIGHT, KEY_CODE_L_SHOT, KEY_CODE_R_SHOT, KEY_CODE_UP, GAME_PAD_DOWN, GAME_PAD_LEFT, GAME_PAD_RIGHT, GAME_PAD_L_SHOT, GAME_PAD_R_SHOT, GAME_PAD_UP } from "../Game"
 import { Actor } from "../actor/Actor"
+import { LaserAppearance } from "../appearance/LaserAppearance"
 import { RotateAppearance } from "../appearance/RotateAppearance"
 import { SimpleAppearance } from "../appearance/SimpleAppearance"
 import { InputManager } from "../useInputManager"
 import { BulletRoutine } from "./BulletRoutine"
+import { LaserRoutine } from "./LaserRoutine"
 import { RoutineBase } from "./RoutineBase"
 
 export class PlayerRoutine extends RoutineBase {
   constructor (private inputManager: InputManager) {
     super()
   }
-  *generateLFire () {
+  *generateNormalShot () {
+    yield
     let nextFireFrame = 0
     while (true) {
       if (nextFireFrame === 0) {
@@ -24,7 +27,8 @@ export class PlayerRoutine extends RoutineBase {
       yield
     }
   }
-  *generateRFire () {
+  *generateWideShot () {
+    yield
     let nextFireFrame = 0
     while (true) {
       if (nextFireFrame === 0) {
@@ -38,9 +42,30 @@ export class PlayerRoutine extends RoutineBase {
       yield
     }
   }
+  *generateLaser (): any {
+    let bullet = null
+    while (true) {
+      const buttonState = yield
+      if (buttonState === 'pressed') {
+        if (bullet == null) {
+          bullet = new Actor(new LaserAppearance(this.actor), new LaserRoutine(this.actor), this.actor.game)
+          bullet.position.set(this.actor.x, this.actor.y)
+          this.actor.game.addActor(bullet, 'playerShot')
+        }
+      } else if (buttonState === 'released') {
+        if (bullet != null) {
+          this.actor.game.removeActor(bullet, 'playerShot')
+          bullet = null
+        }
+      }
+    }
+  }
   *generateUpdate () {
-    const lFire = this.generateLFire()
-    const rFire = this.generateRFire()
+    const lFire = this.generateNormalShot()
+    lFire.next()
+    const rFire = this.generateLaser()
+    rFire.next()
+    // rFire.next()
     while (true) {
       if (this.inputManager.isPressing(KEY_CODE_LEFT) || this.inputManager.isPressingGamepad(GAME_PAD_LEFT)) {
         this.actor.x -= 2
@@ -57,8 +82,15 @@ export class PlayerRoutine extends RoutineBase {
       if (this.inputManager.isPressing(KEY_CODE_L_SHOT) || this.inputManager.isPressingGamepad(GAME_PAD_L_SHOT)) {
         lFire.next()
       }
+      else if (this.inputManager.isPressed(KEY_CODE_R_SHOT) || this.inputManager.isPressed(GAME_PAD_R_SHOT)) {
+        rFire.next('pressed')
+      }
       else if (this.inputManager.isPressing(KEY_CODE_R_SHOT) || this.inputManager.isPressingGamepad(GAME_PAD_R_SHOT)) {
-        rFire.next()
+        rFire.next('pressing')
+      }
+      else if (this.inputManager.isReleased(KEY_CODE_R_SHOT) || this.inputManager.isReleased(GAME_PAD_R_SHOT)) {
+        // ボタンが離されたときにレーザーを消す
+        rFire.next('released')
       }
       yield
     }
